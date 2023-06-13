db45cbaf8ad1a37975ccdcc19664823b3045d0bc
Evgeny Shchekoldin <evgeny@shchekoldin.ru>
2023-06-14 03:07:47 +0600

    made currencies validation

--- internal/api/http/currency.go
+++ internal/api/http/currency.go
@@ -14,6 +14,26 @@ type CurrencyHandler struct {
 	apiLogsRepository ApiLogsRepository
 }
 
+func inArray(target string, array []string) bool {
+	for _, value := range array {
+		if value == target {
+			return true
+		}
+	}
+	return false
+}
+
+func validateSlugs(slugs []string) bool {
+	validSlugs := []string{"USD", "EUR", "RUB", "JPY"}
+	for _, slug := range slugs {
+		if !inArray(slug, validSlugs) {
+			return false
+		}
+	}
+
+	return true
+}
+
 func (h *CurrencyHandler) GetCurrencies(c echo.Context) error {
 	log.Println("_____________________get currencies______________________")
 
@@ -25,13 +45,17 @@ func (h *CurrencyHandler) GetCurrencies(c echo.Context) error {
 		log.Printf("CurrencyHandler.GetCurrencies create api logs due err: %v", err)
 	}
 
-	date := c.QueryParam("date")
 	currencies := strings.Split(c.QueryParam("currencies"), ",")
+	if !validateSlugs(currencies) {
+		log.Printf("CurrencyHandler.GetCurrencies invalid currencies: %v", currencies)
+		return echo.NewHTTPError(http.StatusBadRequest, "invalid currencies")
+	}
 
+	date := c.QueryParam("date")
 	parsedTime, err := time.Parse("2006-01-02", date)
 	if err != nil {
 		log.Printf("CurrencyHandler.GetCurrencies parse time due err: %v", err)
-		return echo.NewHTTPError(http.StatusBadRequest, err)
+		return echo.NewHTTPError(http.StatusBadRequest, "invalid date")
 	}
 	byDate, err := h.repository.GetBySlug(currencies, parsedTime)
 	if err != nil {
@@ -53,14 +77,19 @@ func (h *CurrencyHandler) GetCurrenciesFromTo(c echo.Context) error {
 		log.Printf("CurrencyHandler.GetCurrenciesFromTo create api logs due err: %v", err)
 	}
 
-	date := c.QueryParam("date")
 	currencies := strings.Split(c.QueryParam("currencies"), ",")
+	if !validateSlugs(currencies) {
+		log.Printf("CurrencyHandler.GetCurrencies invalid currencies: %v", currencies)
+		return echo.NewHTTPError(http.StatusBadRequest, "invalid currencies")
+	}
+
+	date := c.QueryParam("date")
 	base := c.QueryParam("base")
 
 	parsedTime, err := time.Parse("2006-01-02", date)
 	if err != nil {
 		log.Printf("CurrencyHandler.GetCurrenciesFromTo parse time due err: %v", err)
-		return echo.NewHTTPError(http.StatusBadRequest, err)
+		return echo.NewHTTPError(http.StatusBadRequest, "invalid date")
 	}
 
 	byDate, err := h.repository.GetBySlugAndBase(currencies, base, parsedTime)
